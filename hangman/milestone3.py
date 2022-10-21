0a2f7225704c41ece31bf46463ab922f568b9260
LiamJPOS <liam.oshaughnessy@live.co.uk>
2022-10-21 18:32:09 +0100

    Milestone 3 Task 1 complete

---
+++ hangman/milestone3.py
@@ -0,0 +1,7 @@
+while True:
+    guess = input("please enter a letter: ")
+    if len(guess) == 1 and guess.isalpha():
+        print("Good guess!")
+        break
+    else:
+        print("Invalid letter. Please, enter a single alphabetical character.")
