d8d27f11adc95ecec42d6e9ae6fb39229ff57fbe
liam <liam.oshaughnessy@live.co.uk>
2022-10-23 12:22:12 +0100

    Solved most of default arguments practical

--- milestone3_practicals.py
+++ milestone3_practicals.py
@@ -1,4 +1,10 @@
 #Void Functions return None
+from email.policy import default
+from sre_compile import isstring
+
+from attr import attributes
+
+
 def void_function():
     print("This is a void function")
 
@@ -24,14 +30,64 @@ def volume_of_sphere(radius):
     
 print(volume_of_sphere(5))
 
+#%%
+#if list is passed in for specific keys, print those keys, otherwise print all
+
 #Default Arguments
-def default_arguments(attributes_to_print):
-    print(attributes_to_print)
+def print_attributes(item_of_clothing , attributes_to_print = 'all'):
+    attributes = []
     
+    if isinstance(attributes_to_print, str):
+    
+        if attributes_to_print == 'all':
+            for i in item_of_clothing:
+                attributes.append(i)
+        
+        if attributes_to_print != 'all':
+            split_attributes = attributes_to_print.split(",")
+            attributes = [i.strip() for i in split_attributes]
+        
+    if isinstance(attributes_to_print, list):
+        attributes = attributes_to_print
+        
+    result = ''       
+    spacer = ''         
+    for attribute in attributes:
+        try:
+            result = result + spacer + str(item_of_clothing[attribute])
+        except KeyError:
+           result = result + spacer + 'attribute ' + str(attribute) + "is not found" 
+        
+        spacer = ', '
+                
+    print(result)
+       
 cardigan = {
     "brand": "Topman", 
     "material": "wool",
     "colour" : "grey"
     }
+'''
+Test Cases for default arguments
 
-default_arguments(cardigan)
+#print_attributes({'one':1, 'two':2, 'three':3})
+# '1, 2, 3'
+
+#print_attributes({'one':1, 'two':2, 'three':3}, 'all')
+# '1, 2, 3'
+
+#print_attributes({'one':1, 'two':2, 'three':3}, ['one', 'three'])
+# '1, 3'
+
+#print_attributes({'one':1, 'two':2, 'three':3}, 'one, three')
+# '1, 3'
+
+#print_attributes({'one':1, 'two':2, 'three':3}, 'one,three')
+# '1, 3'
+
+print_attributes({'one':1, 'two':2, 'three':3}, ['one', 'four', 'three'])
+# '1'
+
+#print_attributes({}, ['one'])
+# ''
+'''
