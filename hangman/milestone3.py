54b7b949a6beebe0eb8ab7d7e0cb1a2537fe6834
liam <liam.oshaughnessy@live.co.uk>
2022-10-31 09:54:39 +0000

    Completes task 3 milestone 3

--- hangman/milestone3.py
+++ hangman/milestone3.py
@@ -3,18 +3,22 @@ import random
 word_list = ["strawberry", "blueberry", "banana", "kiwi", "grape"]
 word = random.choice(word_list)
 
-#Task 1 Iteratively check if input is a valid guess
-while True:
-    guess = input("please enter a letter: ")
-    if len(guess) == 1 and guess.isalpha():
-        break
+#Task 3 Create functions to run the code checks
+def check_guess(guess):
+    check = guess.lower()
+    if check in word:
+        print(f"Good guess! {guess} is in the word")
     else:
-        print("Invalid letter. Please, enter a single alphabetical character.")
+        print(f"Sorry, {guess} is not in the word. Try again.")
 
-#Task 2 Check whether the guess is in the word
-if guess in word:
-    print(f"Good guess! {guess} is in the word")
-else:
-    print(f"Sorry, {guess} is not in the word. Try again.")
+def ask_for_input():
+    while True:
+        guess = input("please enter a letter: ")
+        if len(guess) == 1 and guess.isalpha():
+            break
+        else:
+            print("Invalid letter. Please, enter a single alphabetical character.")
+    
+    check_guess(guess)
 
-
+ask_for_input()
