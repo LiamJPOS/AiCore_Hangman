de0a65bdaaf6f9514bf5fa3db2dbda84b6ffadc6
liam <liam.oshaughnessy@live.co.uk>
2022-11-17 11:27:40 +0000

    Completes task 4 of milestone 4.

--- hangman/milestone4.py
+++ hangman/milestone4.py
@@ -37,7 +37,12 @@ class Hangman():
             self.num_letters -= times_appeared
                     
         else:
+            self.num_lives -= 1
             print(f"Sorry, {guess} is not in the word. Try again.")
+            print(f"You have {self.num_lives} lives left.")
+            
+        self.list_of_guesses.append(guess)
+            
             
     def ask_for_input(self):
         while True:
@@ -51,7 +56,7 @@ class Hangman():
                         
             else:
                 self.check_guess(guess)
-                self.list_of_guesses.append(guess)
+                
                 
                 print(self.word_guessed)
                 print(f"There are {self.num_letters} unique letters left.")
